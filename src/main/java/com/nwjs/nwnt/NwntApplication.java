73c204b73603075d42647b98051c27bd4909188c
KimJaegon <wrohs.k@gmail.com>
2025-07-01 02:06:02 +0900

    init project

---
+++ src/main/java/com/nwjs/nwnt/NwntApplication.java
@@ -0,0 +1,13 @@
+package com.nwjs.nwnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NwntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NwntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nwjs/nwnt/NwntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nwjs.nwnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NwntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
